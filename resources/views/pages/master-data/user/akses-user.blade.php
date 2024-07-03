0944f0ac290d6af5553d2b0918bdbe2c825aed75
fizardev <fizardev@gmail.com>
2024-07-03 17:57:15 +0700

    user akses on progres

--- resources/views/pages/master-data/user/akses-user.blade.php
+++ resources/views/pages/master-data/user/akses-user.blade.php
@@ -1,5 +1,13 @@
 @extends('inc.layout')
 @section('title', 'User Akses')
+@section('extended-css')
+    <style>
+        .custom-control-input:checked~.custom-control-label::before {
+            background: #fd3995;
+            border-color: #cc2875;
+        }
+    </style>
+@endsection
 @section('content')
     <main id="js-page-content" role="main" class="page-content">
         <div class="row">
@@ -12,15 +20,37 @@
                     </div>
                     <div class="panel-container show">
                         <div class="panel-content">
-                            <div class="row">
+                            <div class="card-columns">
                                 @foreach ($roles as $row)
-                                    <div class="col-md-4">
-                                        <h3>{{ $row->name }}</h3>
-                                        @if ($row->permissions->count() > 0)
-                                            @foreach ($row->permissions as $col)
-                                                <span>{{ $col->name }}</span>
-                                            @endforeach
-                                        @endif
+                                    <div class="card border">
+                                        <!-- notice the additions of utility paddings and display properties on .card-header -->
+                                        <div class="card-header bg-primary-500 d-flex pr-2 align-items-center flex-wrap">
+                                            <!-- we wrap header title inside a span tag with utility padding -->
+                                            <div class="card-title font-weight-bold">{{ $row->name }}</div>
+                                            <div class="custom-control d-flex custom-switch ml-auto">
+                                                <input id="demo-switch-{{ $row->id }}" type="checkbox"
+                                                    class="custom-control-input" checked="checked">
+                                                <label class="custom-control-label fw-500"
+                                                    for="demo-switch-{{ $row->id }}"></label>
+                                            </div>
+                                        </div>
+                                        <div class="card-body d-flex pr-2 align-items-center flex-wrap">
+                                            @if ($row->permissions->count() > 0)
+                                                @foreach ($row->permissions as $col)
+                                                    <div class="permissions mr-1">
+                                                        <span>{{ $col->name }}</span><br>
+                                                    </div>
+                                                    <div class="custom-control d-flex custom-switch">
+                                                        <input id="demo-switch-{{ $row->id }}" type="checkbox"
+                                                            class="custom-control-input" checked="checked">
+                                                        <label class="custom-control-label fw-500"
+                                                            for="demo-switch-{{ $row->id }}"></label>
+                                                    </div>
+                                                @endforeach
+                                            @else
+                                                <span>No permissions assigned</span>
+                                            @endif
+                                        </div>
                                     </div>
                                 @endforeach
                             </div>
