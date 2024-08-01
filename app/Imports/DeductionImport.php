f05f29db1bce2c280de0f9bf24238268891914fc
Dmscndraa <72033896+Dimascndraa@users.noreply.github.com>
2024-08-01 15:06:40 +0700

    update data deduction

--- app/Imports/DeductionImport.php
+++ app/Imports/DeductionImport.php
@@ -17,7 +17,6 @@ class DeductionImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
 
-        dd($row);
         // Cari karyawan berdasarkan nama lengkap
         $employee = Employee::where('email', $row['fullname'])->first();
 
@@ -40,15 +39,14 @@ class DeductionImport implements ToModel, WithHeadingRow
         $employee->deduction()->updateOrCreate(
             ['employee_id' => $employee->id],
             [
-                'potongan_keterlambatan' => $row['potongan_keterlambatan'] ?? 0,
-                'potongan_izin' => $row['potongan_izin'] ?? 0,
-                'potongan_sakit' => $row['potongan_sakit'] ?? 0,
-                'simpanan_pokok' => $row['simpanan_pokok'] ?? 0,
-                'potongan_koperasi' => $row['potongan_koperasi'] ?? 0,
-                'potongan_absensi' => $row['potongan_absensi'] ?? 0,
-                'potongan_bpjs_kesehatan' => $row['potongan_bpjs_kesehatan'] ?? 0,
-                'potongan_bpjs_ketenagakerjaan' => $row['potongan_bpjs_ketenagakerjaan'] ?? 0,
-                'potongan_pajak' => $row['potongan_pajak'] ?? 0
+                'potongan_keterlambatan' => $row['potongan_keterlambatan'],
+                'potongan_izin' => $row['potongan_izin'],
+                'simpanan_pokok' => $row['simpanan_pokok'],
+                'potongan_koperasi' => $row['potongan_koperasi'],
+                'potongan_absensi' => $row['potongan_absensi'],
+                'potongan_bpjs_kesehatan' => $row['potongan_bpjs_kesehatan'],
+                'potongan_bpjs_ketenagakerjaan' => $row['potongan_bpjs_ketenagakerjaan'],
+                'potongan_pajak' => $row['potongan_pajak']
             ]
         );
 
