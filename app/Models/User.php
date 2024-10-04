afdd4754d89bfd3ef2fbe466ca8ff997dcb52dfa
fizardev <fizardev@gmail.com>
2024-10-04 15:16:03 +0700

    auditable

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,16 +8,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-// use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Contracts\Auditable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Auditable
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles {
         HasRoles::hasPermissionTo as traitHasPermissionTo;
     }
 
-    // use \OwenIt\Auditing\Auditable;
+    use \OwenIt\Auditing\Auditable;
 
     /**
      * The attributes that are mass assignable.
