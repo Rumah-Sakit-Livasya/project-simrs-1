7bcd3f33a90447240e0b57d4db56dbd0c0b9cfc7
Dimascndraa <dimascndraa18@gmail.com>
2024-11-07 10:45:39 +0700

    schedulling backup

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,13 @@
 
 use App\Console\Commands\NotifyContractExpiry;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\Schedule as ScheduleCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+ScheduleCommand::command('backup:clean')->daily()->at('00:00');
+ScheduleCommand::command('backup:run')->daily()->at('00:30');
