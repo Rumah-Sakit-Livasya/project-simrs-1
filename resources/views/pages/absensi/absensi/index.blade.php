e0cdd43f342119c36c4822e8516e774d1557383e
Fizardev <fizardev@gmail.com>
2024-12-26 16:10:59 +0700

    waktu

--- resources/views/pages/absensi/absensi/index.blade.php
+++ resources/views/pages/absensi/absensi/index.blade.php
@@ -1110,6 +1110,20 @@
         //     });
         // });
 
+        $(document).ready(function() {
+            setInterval(function() {
+                var currentTime = new Date();
+                var hours = currentTime.getHours();
+                var minutes = currentTime.getMinutes();
+                var seconds = currentTime.getSeconds();
+                hours = (hours < 10 ? "0" : "") + hours;
+                minutes = (minutes < 10 ? "0" : "") + minutes;
+                seconds = (seconds < 10 ? "0" : "") + seconds;
+                var timeString = hours + ':' + minutes + ':' + seconds;
+                $("#waktu-realtime").text(timeString);
+            }, 1000);
+        });
+
         document.addEventListener('DOMContentLoaded', () => {
             const video = document.getElementById('video');
             const canvas = document.getElementById('canvas');
