d3bb02ec84ab9bcee570f90cc12ca034ae34f3b6
Fizardev <fizardev@gmail.com>
2025-02-22 10:15:22 +0700

    test

--- app/Http/Controllers/BotMessageController.php
+++ app/Http/Controllers/BotMessageController.php
@@ -36,7 +36,7 @@ class BotMessageController extends Controller
             return response()->json(['error' => 1, 'data' => 'gagal proses'], 403);
         }
 
-        $response = json_encode($content, JSON_PRETTY_PRINT);
+        $response = 'Halo ini fizar';
 
         // Logika bisnis
         $msg = $content['message'] ?? '';
