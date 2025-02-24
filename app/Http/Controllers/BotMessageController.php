fbedc5ffbe350284b46c17ef58733bc7a8269c19
Fizardev <fizardev@gmail.com>
2025-02-24 09:52:11 +0700

    fix wa bot info dayoff

--- app/Http/Controllers/BotMessageController.php
+++ app/Http/Controllers/BotMessageController.php
@@ -450,7 +450,7 @@ class BotMessageController extends Controller
 
                     // Mengirim request HTTP menggunakan cURL
                     $curl = curl_init();
-                    curl_setopt($curl, CURLOPT_URL, 'http://192.168.0.100:3001/send-message');
+                    curl_setopt($curl, CURLOPT_URL, 'http://192.168.0.131:3001/send-message');
                     curl_setopt($curl, CURLOPT_TIMEOUT, 30);
                     curl_setopt($curl, CURLOPT_POST, 1);
                     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -479,7 +479,7 @@ class BotMessageController extends Controller
 
                 // Mengirim request HTTP menggunakan cURL
                 $curl = curl_init();
-                curl_setopt($curl, CURLOPT_URL, 'http://192.168.0.100:3001/send-message');
+                curl_setopt($curl, CURLOPT_URL, 'http://192.168.0.131:3001/send-message');
                 curl_setopt($curl, CURLOPT_TIMEOUT, 30);
                 curl_setopt($curl, CURLOPT_POST, 1);
                 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
