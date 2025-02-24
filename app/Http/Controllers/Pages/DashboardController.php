15a389cd388b3d037a571ca27f7873a3a2acd63a
Fizardev <fizardev@gmail.com>
2025-02-24 10:06:57 +0700

    all requests fix

--- app/Http/Controllers/Pages/DashboardController.php
+++ app/Http/Controllers/Pages/DashboardController.php
@@ -1326,8 +1326,8 @@ class DashboardController extends Controller
                 $query->whereBetween('start_date', [$startDate, $endDate])
                     ->orWhereBetween('end_date', [$startDate, $endDate])
                     ->orWhere(function ($query) use ($startDate, $endDate) {
-                        $query->where('start_date', '<=', $startDate)
-                            ->where('end_date', '>=', $endDate);
+                        $query->where('start_date', '=', $startDate)
+                            ->where('end_date', '=', $endDate);
                     });
             })
                 ->get();
