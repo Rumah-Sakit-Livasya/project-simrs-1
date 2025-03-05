75ac46927543fa37dbe795e80f04397f88952f5c
Fizardev <fizardev@gmail.com>
2025-03-05 10:27:34 +0700

    add log query

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use App\Console\Commands\NotifyContractExpiry;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,5 +21,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        DB::listen(function ($query) {
+            Log::channel('query_action')->debug('Query executed', [
+                'sql' => $query->sql,
+                'bindings' => $query->bindings,
+                'time' => $query->time . ' ms',
+            ]);
+        });
+    }
 }
