f97d6f52ddd50a25793e2ee9dc78c4a00e7e8b20
Fizardev <fizardev@gmail.com>
2025-03-14 15:47:32 +0700

    tambah form pengkajian lanjutan

--- resources/views/pages/simrs/master-data/form-builder/tambah.blade.php
+++ resources/views/pages/simrs/master-data/form-builder/tambah.blade.php
@@ -14,44 +14,8 @@
         .dataTables_scrollFootInner {
             width: 100% !important;
         }
-
-        #filter-wrapper .form-group {
-            display: flex;
-            align-items: center;
-        }
-
-        #filter-wrapper .form-label {
-            margin-bottom: 0;
-            width: 100px;
-            /* Atur lebar label agar semua label sejajar */
-        }
-
-        #filter-wrapper .form-control {
-            flex: 1;
-        }
-
-        @media (max-width: 767.98px) {
-            .custom-margin {
-                margin-top: 15px;
-            }
-
-            #filter-wrapper .form-group {
-                flex-direction: column;
-                align-items: flex-start !important;
-            }
-
-            #filter-wrapper .form-label {
-                width: auto;
-                /* Biarkan lebar label mengikuti konten */
-                margin-bottom: 0.5rem;
-            }
-
-            #filter-wrapper .form-control {
-                width: 100%;
-            }
-        }
     </style>
-    <style src="{{asset('summernote-0.9.0/summernote-bs4.min.css')}}"></style>
+    <style src="{{ asset('summernote-0.9.0/summernote-bs4.min.css') }}"></style>
 @endsection
 @section('content')
     <main id="js-page-content" role="main" class="page-content">
@@ -60,41 +24,41 @@
                 <div id="panel-1" class="panel">
                     <div class="panel-hdr">
                         <h2>
-                            Proses Formulir</span>
+                            Buat Formulir</span>
                         </h2>
                     </div>
                     <div class="panel-container show">
                         <div class="panel-content" id="filter-wrapper">
 
-                            <form action="#" method="get">
+                            <form action="javascript:void(0)" method="POST">
                                 @csrf
                                 <div class="row">
                                     <div class="col-md-4 mb-3">
-                                        <div class="form-group d-flex align-items-center">
-                                            <label for="nama_tindakan_1" class="form-label">Nama Formulir</label>
-                                            <input type="text" width="100%" name="nama_tindakan" id="nama_tindakan_1"
-                                                class="form-control rounded-0 border-top-0 border-left-0 border-right-0 p-0">
+                                        <div class="form-group">
+                                            <label for="nama_tindakan" class="form-label">Nama Formulir</label>
+                                            <input type="text" name="nama_tindakan" id="nama_tindakan"
+                                                class="form-control">
                                         </div>
                                     </div>
                                     <div class="col-md-4 mb-3">
-                                        <div class="form-group d-flex align-items-center">
-                                            <label for="nama_tindakan_1" class="form-label">Kategori Formulir</label>
-                                            <input type="text" width="100%" name="nama_tindakan" id="nama_tindakan_1"
-                                                class="form-control rounded-0 border-top-0 border-left-0 border-right-0 p-0">
+                                        <div class="form-group">
+                                            <label for="kategori_formulir" class="form-label">Kategori Formulir</label>
+                                            <input type="text" name="kategori_formulir" id="kategori_formulir"
+                                                class="form-control">
                                         </div>
                                     </div>
                                     <div class="col-md-4 mb-3">
-                                        <div class="form-group d-flex align-items-center">
-                                            <label for="nama_tindakan_1" class="form-label">Status</label>
-                                            <select class="select2 form-control @error('perawat_id') is-invalid @enderror"
-                                                name="perawat_id" id="perawat_id">
+                                        <div class="form-group">
+                                            <label for="status" class="form-label">Status</label>
+                                            <select class="form-control" name="status" id="status">
                                                 <option value="1">Aktif</option>
                                                 <option value="0">Tidak Aktif</option>
                                             </select>
                                         </div>
                                     </div>
                                     <div class="col-md-12 mb-3">
-                                        <textarea name="form_source" id="summernote"></textarea>
+                                        <label for="summernote" class="form-label mb-2">Isi Formulir</label>
+                                        <textarea name="form_source" id="summernote" class="form-control"  rows="10"></textarea>
                                     </div>
                                     <div class="col-md-12">
                                         <button type="submit" class="btn btn-block mt-2 btn-primary">
@@ -103,6 +67,7 @@
                                     </div>
                                 </div>
                             </form>
+
                         </div>
                     </div>
                 </div>
@@ -115,14 +80,14 @@
     <script src="/js/datagrid/datatables/datatables.bundle.js"></script>
     <script src="/js/datagrid/datatables/datatables.export.js"></script>
     <script src="/js/formplugins/select2/select2.bundle.js"></script>
-    
-    <script src="{{asset('summernote-0.9.0/summernote-bs4.min.js')}}"></script>
+
+    <script src="{{ asset('summernote-0.9.0/summernote-bs4.min.js') }}"></script>
     <script>
         $(document).ready(function() {
             $('.select2').select2();
 
             $('#summernote').summernote(); //text editor
-            
+
             $('#loading-spinner').show();
 
             $('#store-form').on('submit', function(e) {
