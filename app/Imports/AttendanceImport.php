43ef205e76be8187dba223ec6f00ce25356ee748
Dimascndra <dimascndraa18@gmail.com>
2025-06-13 10:19:46 +0700

    Attendance Import

--- app/Imports/AttendanceImport.php
+++ app/Imports/AttendanceImport.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class LaporanInternalImport implements ToCollection, WithHeadingRow
+class AttendanceImport implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
