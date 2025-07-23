0dfeec108537b09508cbe3402ce4b267263871d7
Dimascndra <dimascndraa18@gmail.com>
2025-07-23 17:55:19 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -353,7 +353,7 @@ Route::middleware(['web', 'auth'])->prefix('dashboard')->group(function () {
 
 Route::get('/tts', [TextToSpeechController::class, 'tts'])->name('tts');
 Route::post('livasya-message', [BotMessageController::class, 'livasyaMessage'])->middleware(CheckAuthorizationBot::class)->name('bot.verified');
-Route::post('process-message', [BotMessageController::class, 'processMessage'])->middleware(CheckAuthorizationBot::class)->name('bot.kirim-pesan');
+Route::post('process-message', [WhatsappController::class, 'processMessage'])->middleware(CheckAuthorizationBot::class)->name('bot.kirim-pesan');
 Route::post('notify-contract', [BotMessageController::class, 'notifyExpiryContract'])->middleware(CheckAuthorizationBot::class);
 // Route::get('notify-contract', [BotMessageController::class, 'notifyExpiryContract']);
 
