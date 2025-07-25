26fb2ec2e853b082c9ada98f8bd6fefa63986d77
AdibSyahna <adib.syahna@gmail.com>
2025-07-25 10:28:26 +0700

    added models: farmasi resep elektronik + items, farmasi resep + items, farmasi resep response

---
+++ database/migrations/2025_07_24_063755_create_farmasi_resep_elektroniks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    use SoftDeletes;
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('farmasi_resep_elektroniks', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('user_id')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('registration_id')->nullable()->constrained('registrations')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('gudang_id')->constrained('warehouse_master_gudang')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('kode_re')->unique();
+            $table->string('resep_manual')->nullable();
+            $table->integer('total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('farmasi_resep_elektroniks');
+    }
+};
