030c16284f7d1f2159d8c58929bfd559ba2db6a8
Dimascndra <dimascndraa18@gmail.com>
2025-08-09 09:31:41 +0700

    fix:search laporan internal

--- resources/views/pages/laporan-internal/index.blade.php
+++ resources/views/pages/laporan-internal/index.blade.php
@@ -134,17 +134,17 @@
 
     <script>
         // SweetAlert2 Toast Configuration
-        // const Toast = Swal.mixin({
-        //     toast: true,
-        //     position: 'top-end',
-        //     showConfirmButton: false,
-        //     timer: 3000,
-        //     timerProgressBar: true,
-        //     didOpen: (toast) => {
-        //         toast.addEventListener('mouseenter', Swal.stopTimer);
-        //         toast.addEventListener('mouseleave', Swal.resumeTimer);
-        //     }
-        // });
+        var Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer);
+                toast.addEventListener('mouseleave', Swal.resumeTimer);
+            }
+        });
 
         function showToast(message, icon = 'success') {
             Toast.fire({
