3e3fd3f9a782064d35fbd067f7d3ca646fd2e2de
rickyfauzi <rickyfauzi116@gmail.com>
2025-09-13 10:28:42 +0700

    Add persalinan master-data views from e-finance branch

---
+++ resources/views/pages/simrs/master-data/persalinan/daftar-persalinan/index.blade.php
@@ -0,0 +1,391 @@
+@extends('inc.layout')
+@section('title', 'Daftar Persalinan')
+@section('extended-css')
+    <style>
+        hr {
+            border: 1px dashed #fd3995 !important;
+        }
+
+        div.table-responsive>div.dataTables_wrapper>div.row>div[class^="col-"]:last-child {
+            padding: 0px;
+        }
+
+        .dataTables_scrollHeadInner,
+        .dataTables_scrollFootInner {
+            width: 100% !important;
+        }
+
+        #filter-wrapper .form-group {
+            display: flex;
+            align-items: center;
+        }
+
+        #filter-wrapper .form-label {
+            margin-bottom: 0;
+            width: 100px;
+            /* Atur lebar label agar semua label sejajar */
+        }
+
+        #filter-wrapper .form-control {
+            flex: 1;
+        }
+
+        @media (max-width: 767.98px) {
+            .custom-margin {
+                margin-top: 15px;
+            }
+
+            #filter-wrapper .form-group {
+                flex-direction: column;
+                align-items: flex-start !important;
+            }
+
+            #filter-wrapper .form-label {
+                width: auto;
+                /* Biarkan lebar label mengikuti konten */
+                margin-bottom: 0.5rem;
+            }
+
+            #filter-wrapper .form-control {
+                width: 100%;
+            }
+        }
+    </style>
+@endsection
+@section('content')
+    <main id="js-page-content" role="main" class="page-content">
+        <div class="row justify-content-center">
+            <div class="col-xl-10">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Form Pencarian</span>
+                        </h2>
+                    </div>
+                    <div class="panel-container show">
+                        <div class="panel-content" id="filter-wrapper">
+
+                            <form action="/daftar-rekam-medis" method="get">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-md-12 mb-3">
+                                        <div class="form-group d-flex align-items-center">
+                                            <label for="nama_tindakan_1" class="form-label">Nama</label>
+                                            <input type="text" name="nama_tindakan" id="nama_tindakan_1"
+                                                class="form-control rounded-0 border-top-0 border-left-0 border-right-0 p-0">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn btn-sm float-right mt-2 btn-primary">
+                                            <i class="fas fa-search mr-1"></i> Cari
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xl-12">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Daftar Persalinan
+                        </h2>
+                    </div>
+                    <div class="panel-container show">
+                        <div class="panel-content">
+                            <!-- datatable start -->
+                            <div class="table-responsive">
+                                <table id="dt-basic-example" class="table table-bordered table-hover table-striped w-100">
+                                    <i id="loading-spinner" class="fas fa-spinner fa-spin"></i>
+                                    <thead class="bg-primary-600">
+                                        <tr>
+                                            <th>Kode</th>
+                                            <th>Nama Persalinan</th>
+                                            <th>Nama Billing</th>
+                                            <th>Fungsi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($daftar_persalinan as $row)
+                                            <tr>
+                                                <td>{{ $row->kode }}</td>
+                                                <td>{{ $row->nama_persalinan }}</td>
+                                                <td>{{ $row->nama_billing }}</td>
+                                                <td>
+                                                    <button class="btn btn-sm btn-primary px-2 py-1 btn-tarif"
+                                                        data-id="{{ $row->id }}">
+                                                        <i class="fas fa-credit-card"></i>
+                                                    </button>
+                                                    <button class="btn btn-sm btn-success px-2 py-1 btn-edit"
+                                                        data-id="{{ $row->id }}">
+                                                        <i class="fas fa-pencil"></i>
+                                                    </button>
+                                                    <button class="btn btn-sm btn-danger px-2 py-1 btn-delete"
+                                                        data-id="{{ $row->id }}">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th colspan="4" class="text-center">
+                                                <button type="button"
+                                                    class="btn btn-outline-primary waves-effect waves-themed"
+                                                    id="btn-tambah-persalinan">
+                                                    <span class="fal fa-plus-circle"></span>
+                                                    Tambah Daftar Persalinan
+                                                </button>
+                                            </th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- datatable end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    @include('pages.simrs.master-data.persalinan.daftar-persalinan.partials.create')
+    @include('pages.simrs.master-data.persalinan.daftar-persalinan.partials.edit')
+@endsection
+@section('plugin')
+    <script src="/js/datagrid/datatables/datatables.bundle.js"></script>
+    <script src="/js/datagrid/datatables/datatables.export.js"></script>
+    <script src="/js/formplugins/select2/select2.bundle.js"></script>
+    <script>
+        $(document).ready(function() {
+            let persalinanId = null;
+            $('#loading-spinner').show();
+
+            $('#modal-tambah-persalinan .select2').select2({
+                dropdownParent: $('#modal-tambah-persalinan')
+            });
+
+            $('#btn-tambah-persalinan').click(function() {
+                $('#modal-tambah-persalinan').modal('show');
+            });
+
+            $('.btn-tarif').click(function() {
+                const id_param = $(this).attr('data-id');
+                const url = `{{ route('master-data.persalinan.tarif.index', ':id') }}`
+                    .replace(':id', id_param);
+                const popupWidth = 900;
+                const popupHeight = 600;
+                const left = (screen.width - popupWidth) / 2;
+                const top = (screen.height - popupHeight) / 2;
+
+                window.open(
+                    url,
+                    "popupWindow",
+                    "width=" + popupWidth + ",height=" + popupHeight + ",top=" + top + ",left=" + left +
+                    ",scrollbars=yes,resizable=yes"
+                );
+            });
+
+            $('.btn-edit').click(function() {
+                console.log('clicked');
+                $('#modal-edit-persalinan').modal('show');
+                persalinanId = $(this).attr('data-id');
+                $('#modal-edit-persalinan form').attr('data-id', persalinanId);
+
+                $.ajax({
+                    url: '/api/simrs/master-data/persalinan/daftar-persalinan/' +
+                        persalinanId,
+                    type: 'GET',
+                    success: function(response) {
+                        $('#modal-edit-persalinan #tipe_edit').val(response.tipe).select2({
+                            dropdownParent: $('#modal-edit-persalinan')
+                        });
+                        $('#modal-edit-persalinan input[name="kode"]').val(response.kode);
+                        $('#modal-edit-persalinan input[name="nama_persalinan"]').val(response
+                            .nama_persalinan);
+                        $('#modal-edit-persalinan input[name="nama_billing"]').val(response
+                            .nama_billing);
+                    },
+                    error: function(xhr, status, error) {
+                        $('#modal-edit-persalinan').modal('hide');
+                        showErrorAlert('Terjadi kesalahan: ' + error);
+                    }
+                });
+
+            });
+
+            $('.btn-delete').click(function() {
+                var persalinanId = $(this).attr('data-id');
+
+                // Menggunakan confirm() untuk mendapatkan konfirmasi dari pengguna
+                var userConfirmed = confirm('Anda Yakin ingin menghapus ini?');
+
+                if (userConfirmed) {
+                    // Jika pengguna mengklik "Ya" (OK), maka lakukan AJAX request
+                    $.ajax({
+                        url: '/api/simrs/master-data/persalinan/daftar-persalinan/' +
+                            persalinanId +
+                            '/delete',
+                        type: 'DELETE',
+                        success: function(response) {
+                            showSuccessAlert(response.message);
+
+                            setTimeout(() => {
+                                console.log('Reloading the page now.');
+                                window.location.reload();
+                            }, 1000);
+                        },
+                        error: function(xhr, status, error) {
+                            showErrorAlert('Terjadi kesalahan: ' + error);
+                        }
+                    });
+                } else {
+                    console.log('Penghapusan dibatalkan oleh pengguna.');
+                }
+            });
+
+            $('#update-form').on('submit', function(e) {
+                e.preventDefault(); // Mencegah form submit secara default
+
+                var formData = $(this).serialize();
+                persalinanId = $(this).attr('data-id');
+                $.ajax({
+                    url: '/api/simrs/master-data/persalinan/daftar-persalinan/' +
+                        persalinanId +
+                        '/update',
+                    type: 'PATCH',
+                    data: formData,
+                    beforeSend: function() {
+                        $('#update-form').find('.ikon-edit').hide();
+                        $('#update-form').find('.spinner-text').removeClass(
+                            'd-none');
+                    },
+                    success: function(response) {
+                        $('#modal-edit-persalinan').modal('hide');
+                        showSuccessAlert(response.message);
+
+                        setTimeout(() => {
+                            console.log('Reloading the page now.');
+                            window.location.reload();
+                        }, 1000);
+                    },
+                    error: function(xhr, status, error) {
+                        if (xhr.status === 422) {
+                            var errors = xhr.responseJSON.errors;
+                            var errorMessages = '';
+
+                            $.each(errors, function(key, value) {
+                                errorMessages += value +
+                                    '\n';
+                            });
+
+                            $('#modal-edit-persalinan').modal('hide');
+                            showErrorAlert('Terjadi kesalahan:\n' +
+                                errorMessages);
+                        } else {
+                            $('#modal-edit-persalinan').modal('hide');
+                            showErrorAlert('Terjadi kesalahan: ' + error);
+                            console.log(error);
+                        }
+                    }
+                });
+            });
+
+            $('#store-form').on('submit', function(e) {
+                e.preventDefault(); // Mencegah form submit secara default
+
+                var formData = $(this).serialize(); // Mengambil semua data dari form
+
+                $.ajax({
+                    url: '/api/simrs/master-data/persalinan/daftar-persalinan',
+                    type: 'POST',
+                    data: formData,
+                    beforeSend: function() {
+                        $('#store-form').find('.ikon-tambah').hide();
+                        $('#store-form').find('.spinner-text').removeClass(
+                            'd-none');
+                    },
+                    success: function(response) {
+                        $('#modal-tambah-persalinan').modal('hide');
+                        showSuccessAlert(response.message);
+
+                        setTimeout(() => {
+                            console.log('Reloading the page now.');
+                            window.location.reload();
+                        }, 1000);
+                    },
+                    error: function(xhr, status, error) {
+                        if (xhr.status === 422) {
+                            var errors = xhr.responseJSON.errors;
+                            var errorMessages = '';
+
+                            $.each(errors, function(key, value) {
+                                errorMessages += value +
+                                    '\n';
+                            });
+
+                            $('#modal-tambah-persalinan').modal('hide');
+                            showErrorAlert('Terjadi kesalahan:\n' +
+                                errorMessages);
+                        } else {
+                            $('#modal-tambah-persalinan').modal('hide');
+                            showErrorAlert('Terjadi kesalahan: ' + error);
+                            console.log(error);
+                        }
+                    }
+                });
+            });
+
+            // initialize datatable
+            $('#dt-basic-example').DataTable({
+                "drawCallback": function(settings) {
+                    // Menyembunyikan preloader setelah data berhasil dimuat
+                    $('#loading-spinner').hide();
+                },
+                responsive: false, // Responsif diaktifkan
+                scrollX: true, // Tambahkan scroll horizontal
+                lengthChange: false,
+                dom: "<'row mb-3'<'col-sm-12 col-md-6 d-flex align-items-center justify-content-start'f><'col-sm-12 col-md-6 d-flex align-items-center justify-content-end buttons-container'B>>" +
+                    "<'row'<'col-sm-12'tr>>" +
+                    "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+                buttons: [{
+                        extend: 'pdfHtml5',
+                        text: 'PDF',
+                        titleAttr: 'Generate PDF',
+                        className: 'btn-outline-danger btn-sm mr-1 custom-margin'
+                    },
+                    {
+                        extend: 'excelHtml5',
+                        text: 'Excel',
+                        titleAttr: 'Generate Excel',
+                        className: 'btn-outline-success btn-sm mr-1 custom-margin'
+                    },
+                    {
+                        extend: 'csvHtml5',
+                        text: 'CSV',
+                        titleAttr: 'Generate CSV',
+                        className: 'btn-outline-primary btn-sm mr-1 custom-margin'
+                    },
+                    {
+                        extend: 'copyHtml5',
+                        text: 'Copy',
+                        titleAttr: 'Copy to clipboard',
+                        className: 'btn-outline-primary btn-sm mr-1 custom-margin'
+                    },
+                    {
+                        extend: 'print',
+                        text: 'Print',
+                        titleAttr: 'Print Table',
+                        className: 'btn-outline-primary btn-sm custom-margin'
+                    }
+                ]
+            });
+
+        });
+    </script>
+@endsection
