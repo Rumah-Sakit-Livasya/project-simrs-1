d42d20d829975cf8e257f32ebe6ba8929f9a527c
Dimascndra <dimascndraa18@gmail.com>
2025-10-16 15:29:32 +0700

    request hr

--- app/Models/UploadFile.php
+++ app/Models/UploadFile.php
@@ -17,6 +17,6 @@ class UploadFile extends Model
      */
     public function employee(): BelongsTo
     {
-        return $this->belongsTo(Employee::class, 'employee_id');
+        return $this->belongsTo(Employee::class, 'pic');
     }
 }
